bd5926dafa8e72fb4873bcb628b5295ee5d8cc51
Ry4nnnnn <3548399632@qq.com>
2025-02-16 19:23:36 +0800

    新的targethud，并且格式化了一下代码

--- src/minecraft/cn/stars/reversal/ui/atmoic/island/Atomic.java
+++ src/minecraft/cn/stars/reversal/ui/atmoic/island/Atomic.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 /**
  * 原子
+ *
  * @author Stars
  */
 @Getter
@@ -31,14 +32,16 @@ public class Atomic implements GameInstance {
     public static float getRenderX(float x) {
         return x - width / 2;
     }
+
     public static float getRenderY(float y) {
         return y - height / 2;
     }
 
     /**
      * 提交一个自定义渲染任务至AtomicIslandRenderer
-     * @param task 渲染任务
-     * @param timeout 渲染时间
+     *
+     * @param task        渲染任务
+     * @param timeout     渲染时间
      * @param allowRepeat 是否允许重复的任务
      */
     public static void submitTask(Runnable task, long timeout, boolean allowRepeat, int priority) {
@@ -68,10 +71,11 @@ public class Atomic implements GameInstance {
 
     /**
      * 提交一个AtomicIslandRenderer展示的预设消息
+     *
      * @param description 内容
-     * @param title 标题
-     * @param delay 渲染时间
-     * @param icon 标题前的图标
+     * @param title       标题
+     * @param delay       渲染时间
+     * @param icon        标题前的图标
      */
     public static void registerAtomic(final String description, final String title, final long delay, final String icon) {
         registerAtomic(description, title, delay, icon, true);
@@ -95,7 +99,7 @@ public class Atomic implements GameInstance {
         submitTask(() -> {
             Atomic.width = Math.max(psm18.width(description), psb20.width(title) + 10) + 10;
             Atomic.height = 30;
-            FontManager.getIcon(24).drawString(icon, Atomic.INSTANCE.x.getValue() + 3, Atomic.INSTANCE.y.getValue() + 6,  new Color(250, 250, 250, 250).getRGB());
+            FontManager.getIcon(24).drawString(icon, Atomic.INSTANCE.x.getValue() + 3, Atomic.INSTANCE.y.getValue() + 6, new Color(250, 250, 250, 250).getRGB());
             psb20.drawString(title, Atomic.INSTANCE.x.getValue() + 16, Atomic.INSTANCE.y.getValue() + 6, new Color(250, 250, 250, 250).getRGB());
             psm18.drawString(description, Atomic.INSTANCE.x.getValue() + 5, Atomic.INSTANCE.y.getValue() + 18, new Color(250, 250, 250, 250).getRGB());
         }, delay, allowRepeat, priority);
